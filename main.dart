71930f801fff63469d58421141c64db2da34fbd7
Omkar-Rajkumar-Khade <99533461+Omkar-Rajkumar-Khade@users.noreply.github.com>
2022-03-21 20:21:54 +0530

    Basics of Dart

---
+++ main.dart
@@ -0,0 +1,58 @@
+void main() {
+  //##valid names:
+  //var name1 = "Omkar";
+  //var $name = "Rohan";
+  //var g_name = "Atharva";
+
+  //##invalid names
+  //var 1name="jayendra";
+  //var for(inbuid)="soham"
+  //print($name);
+  //print(g_name);
+  //print("Hello Dart " + name1);
+
+  /*##Build in DataTypes:
+ int num1 = 10;
+ double num2 = 10.21;
+ String name = "name";
+ bool true1 = true;
+ print(name);
+ print(true1);
+ print(true1.runtimeType);
+ print(num2);
+ print(num1);*/
+
+  //operators
+  //Arithmatic operators
+
+  // int a = 10;
+  //int b = 20;
+  //print(a - b);
+  //print(a + b);
+  //print(a * b);
+  // print(a / b);
+
+  /*##Relation Operator
+  int a = 100;
+  print b = 200;
+  print(a == b);
+  print(a > b);
+  print(a < b);
+  print(a != b);
+  print(a >= b);
+  print(a <= b);*/
+
+  //type test operators
+  //int a = 10;
+  //print(a is String);
+
+  /*conditional expression
+   var name= true;
+   var user=name? 'soham':'guest';
+  print(user); */
+
+//var name2='sonam';
+//var name3=null;
+//var user= name??'guest';
+//print(user);
+}
