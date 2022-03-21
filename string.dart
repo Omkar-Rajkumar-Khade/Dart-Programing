b6c960ba1ec516f66fe0808b6623db5973ae60b6
Omkar-Rajkumar-Khade <99533461+Omkar-Rajkumar-Khade@users.noreply.github.com>
2022-03-21 20:23:16 +0530

    String In Dart

---
+++ string.dart
@@ -0,0 +1,62 @@
+void main() {
+  //string
+//1st Way
+  //String name2 = "afsdffgdf";
+  //print(name2);
+
+  //2nd way:
+  // var stringname = "asfsfd";
+  // print(stringname);
+  // print(stringname.runtimeType);
+
+  //##way to print multiple line string
+  //var comment = '''thes is
+  //multiple line
+  //string example''';
+  //print(comment);
+  //.toUpperCase use to convert string to uppercase
+  //print(comment.toUpperCase());
+
+  //##string subtopics : Expressions
+  // var subname = 'rahul';
+  // print('${subname}');
+
+  //##string concatination
+  //String s1 = 'omkar';
+  //String s2 = 'khade';
+  //print(s1 + ' ' + s2);
+  //print('hello'+'my name is'+s1+' '+s2);
+  // \n is used to print in next line
+
+  //##string properties
+  //var name = 'Rahul';
+  //to check lenght of string
+  //print(name.length);
+  // to check wheater string is empty
+  //print(name.isEmpty);
+  //to chech wheater string isnt empty
+  //print(name.isNotEmpty);
+  //to check whetear string contains element
+  //print(name.contains('R'));
+  //to convert string to uppercase
+  //print(name.toUpperCase());
+  //to canvert string to lower case
+  //print(name.toLowerCase());
+  // to move string to right side
+  //print(name.padLeft(10));
+  //print(name.padRight(10));
+
+  //##string trim
+  //var name1 = "   Rahul   ";
+  //print(name1);
+  //to remove spaces from both sides
+  //print(name1.trim());
+//to remove space from left side
+  //print(name1.trimLeft());
+  //to remove space from right side
+  //print(name1.trimLeft());
+
+  //##Split String
+  //var name = 'hello&dart';
+  // print(name.split(' '));
+}
