5fd64d3b0005231d80e64f25ee13f2a19d67a9c3
Omkar-Rajkumar-Khade <99533461+Omkar-Rajkumar-Khade@users.noreply.github.com>
2022-03-22 23:12:37 +0530

    While and For Loops in Dart

---
+++ Loop.dart
@@ -0,0 +1,36 @@
+void main() {
+  //##ForLoop
+  //For(initiation;size;increament)
+  //var students = ['omkar', 'rohan', 'sahil', 'pratik'];
+  /*for (int i = 0; i < students.length; i++) {
+    print(students[i]);
+  }*/
+
+  //##While Loop
+  /*int i = 0;
+  while (i < students.length) {
+    print(students[i]);
+    i++;
+    
+  }*/
+
+  //## for in loop with list [same for Set and Map]
+  /*var students = ['omkar', 'pratik', 'ayush', 'jay'];
+  for (var i in students) {
+    print(i);
+  }*/
+
+  //##Break and Continue:
+  /*var var1 = true;
+  var n = 0;
+  while ( var1) {
+    n++;
+    if (n == 10) {
+      break;
+    }
+    if (n == 5) {
+      continue;
+    }
+    print(n);
+  }*/
+}
