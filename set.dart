dc8ea7f2789234c2de3594beb109091a9e7f1c8d
Omkar-Rajkumar-Khade <99533461+Omkar-Rajkumar-Khade@users.noreply.github.com>
2022-03-28 21:39:19 +0530

    sets in dart

---
+++ set.dart
@@ -0,0 +1,13 @@
+//set is a collecton of unorderd value in array.
+//duplicate value not allowed in set
+
+void main() {
+  /* var set1 = {'rahul', 'soham', 'neha', 'rajesh'};
+  print(set1);
+  print(set1.runtimeType);*/
+
+  //##add elements to set
+  var set2 = <String>{};
+  set2.add("java");
+  print(set2);
+}
