90e766b67d8134ecc1c3ba46257fa240342ea40c
Omkar-Rajkumar-Khade <99533461+Omkar-Rajkumar-Khade@users.noreply.github.com>
2022-03-28 21:35:26 +0530

    map in dart

---
+++ map.dart
@@ -0,0 +1,36 @@
+//map is object which contains pair of key and value
+//key should be unique and value may duplicate
+
+void main() {
+//##map literal:
+  /*var adress = {'name': 'sonam', 'city': 'pune', 'state': 'maharastra'};
+  print(adress);
+  print(adress.runtimeType);*/
+
+//method 2:for specific data type
+  /* Map<String, String> address = {
+    'name': 'omkar',
+    'college': 'VIIT',
+    'city': 'pune'
+  };
+  print(address);
+  print(address.runtimeType);
+*/
+
+/*
+  //##create empty Map and element in it
+  var address1 = {};
+  address1['name'] = 'Sonam';
+  address1['city'] = 'Ranchi';
+  print(address1);
+  print(address1.runtimeType);
+*/
+/*
+  //## Access elements of Map
+  var adress3 = {'name': 'Omkar', 'city': 'Pune', 'college': 'viit'};
+  print(adress3['name']);
+  print(adress3['city']);
+  print(adress3['college']);
+*/
+//##Properties:
+}
