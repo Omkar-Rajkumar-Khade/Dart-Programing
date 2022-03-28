b0c4fca198b86cbe065d9ccc1fa859b46343ebf7
Omkar-Rajkumar-Khade <99533461+Omkar-Rajkumar-Khade@users.noreply.github.com>
2022-03-28 21:40:30 +0530

    function in dart

---
+++ functions.dart
@@ -0,0 +1,39 @@
+void main() {
+//##defination of function:
+/*returtype function_name(datatype parameter){
+statements;
+return statements;
+}*/
+  /* addnum() {
+    int a = 45;
+    int b = 34;
+    return a + b;
+  }
+
+  print(addnum());
+}*/
+
+  /*String infofun(String name, String city) {
+    return 'i am $name and from $city';
+  }
+
+  var info = infofun('omkar', 'pune');
+  print(info);*/
+
+  /*int addfun(int a, int b) {
+    return a + b;
+  }
+
+  var addition = addfun(10, 34);
+  print(addition);
+  */
+
+  //##defualt value in function
+  //here we can add defualt value in parameter 
+ /* String infofun(String name, [String city = 'Mumbai']) {
+    return 'i am $name and from $city';
+  }
+
+  var info = infofun('omkar',);
+  print(info);*/
+}
